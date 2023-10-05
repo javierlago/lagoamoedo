8d47fa1c2c7cfe2cd9329570078c5f2d9efc31fb
javierlago <javierlagoamoedo@gmail.com>
2023-10-05 14:21:08 +0200

    trabajando en acerca de

--- eventos.py
+++ eventos.py
@@ -1,7 +1,24 @@
+import sys
+
 import var
 
 class Eventos():
     def salir(self):
+        try:
+            sys.exit(0)
+        except Exception as error:
+            print(error, "en módulo eventos")
+
+
+    @staticmethod
+    def abrirCalendar(self):
+
+        try:
+            var.calendar.show()
+        except Exception as erro:
+            print("erro en abrir",erro)
+
+    def acercade(self):
         try:
             pass
         except Exception as error:
--- main.py
+++ main.py
@@ -1,18 +1,42 @@
+from datetime import datetime
+import sys
+
 import eventos
+import var
+from WindowCalendar import *
 from MainWindow import *
-import sys, var
+
+
+
+
 class Main(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(Main, self).__init__()
         var.ui = Ui_MainWindow()
         var.ui.setupUi(self) #metodo encargado de genera la interfaz
+        var.calendar = Calendar()
+
         '''
         ZONA DE EVENTOS
         
         '''
+        var.ui.btnCalendar.clicked.connect(eventos.Eventos.abrirCalendar)
+        '''
+        zona de eventos salir
+        '''
+        var.ui.actionSalir.triggered.connect(eventos.Eventos.salir)
+
+class Calendar(QtWidgets.QDialog):
+    def __init__(self):
+        super(Calendar, self).__init__()
+        var.calendar = Ui_WindowCalendar()
+        var.calendar.setupUi(self)
 
 
+        dia = datetime.now().day
+        mes = datetime.now().month
+        ano = datetime.now().year
 
 
 if __name__ == '__main__':
